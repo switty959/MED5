83819a6f2ace87e8b08282022eabe854b9ed9967
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-11-10 15:40:14 +0100

    flickering light was made
    Flickering light was made
    - missing sound

---
+++ MED5Project/Assets/flickingLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickingLight : MonoBehaviour
+{
+    Light lightSource;
+    public float minWaitTime, maxWaitTime;
+    void Start()
+    {
+        lightSource = GetComponent<Light>();
+        StartCoroutine(flickering());
+    }
+   
+
+
+
+   IEnumerator flickering()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            lightSource.enabled = !lightSource.enabled;
+        }
+    }
+}
