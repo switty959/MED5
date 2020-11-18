b9e5426f2d0d7321838df1c645f0dd83532a6cf6
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-11-18 12:38:02 +0100

    timer made

---
+++ MED5Project/Assets/timerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerScript : MonoBehaviour
+{
+
+    public bool countdown, timer;
+    public float counter;
+
+    public int maxtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown && !timer)
+        {
+            countdownFunction(maxtime);
+        }
+        if (!countdown && timer)
+        {
+            timerFunction();
+        }
+       
+    }
+    void countdownFunction( float maxTime)
+    {
+        counter = maxTime;
+        counter -= Time.deltaTime;
+        if (counter<0)
+        {
+            Debug.Log("The time is up");
+        }
+    }
+
+    void timerFunction()
+    {
+        counter = Time.time;
+    }
+}
