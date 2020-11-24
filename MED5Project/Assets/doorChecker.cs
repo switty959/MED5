649dfbce512c6ea80051a0566dd2db868ee6c890
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-11-24 13:38:16 +0100

    door should play sound now

--- MED5Project/Assets/doorChecker.cs
+++ MED5Project/Assets/doorChecker.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class doorChecker : MonoBehaviour
 {
     public bool[] doorOpen;
+    public int test;
     public GameObject[] doors = new GameObject[5];
     public Color[] doorlight = new Color[2];
+    public AudioClip doorSoundEffect;
     // Start is called before the first frame update
+
+    // door sound effect : https://www.youtube.com/watch?v=cXqDc6I1NP8
     void Start()
     {
         doorOpen = new bool[doors.Length];
@@ -23,13 +27,31 @@ public class doorChecker : MonoBehaviour
                 doors[i].GetComponent<Renderer>().materials[3].color = doorlight[0];
                 doors[i].GetComponent<Renderer>().materials[4].color = doorlight[0];
                 doors[i].GetComponent<Animator>().SetBool("opening",true);
+
+                StartCoroutine(knockingOnDoor());
+
+                if (test == 0)
+                {
+                    GetComponent<AudioSource>().PlayOneShot(doorSoundEffect);
+                    test++;
+                }
+
+
             }
             else
             {
                 doors[i].GetComponent<Renderer>().materials[3].color = doorlight[1];
                 doors[i].GetComponent<Renderer>().materials[4].color = doorlight[1];
                 doors[i].GetComponent<Animator>().SetBool("opening", false);
+                
             }
         }   
+    }
+
+    IEnumerator knockingOnDoor()
+    {
+    yield return new WaitForSeconds(doorSoundEffect.length);
+    test = 0;
+
     }
 }
