61d7b3f925054103a5a756758c676b5bb18d949a
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-11-25 13:08:29 +0100

    updated the event objects
    trigger box for facescanner and handscanner with flickering light attached

--- MED5Project/Assets/Scripts/doorChecker.cs
+++ MED5Project/Assets/Scripts/doorChecker.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class doorChecker : MonoBehaviour
 {
     public bool[] doorOpen;
-    public int test;
     public GameObject[] doors = new GameObject[5];
+    public GameObject[] teleportplanes;
     public Color[] doorlight = new Color[2];
     public AudioClip doorSoundEffect;
     // Start is called before the first frame update
@@ -15,6 +15,8 @@ public class doorChecker : MonoBehaviour
     void Start()
     {
         doorOpen = new bool[doors.Length];
+        teleportplanes[7].SetActive(true);
+        teleportplanes[8].SetActive(true);
     }
 
     // Update is called once per frame
@@ -27,9 +29,6 @@ public class doorChecker : MonoBehaviour
                 doors[i].GetComponent<Renderer>().materials[3].color = doorlight[0];
                 doors[i].GetComponent<Renderer>().materials[4].color = doorlight[0];
                 doors[i].GetComponent<Animator>().SetBool("opening",true);
-
-
-
             }
             else
             {
@@ -38,7 +37,29 @@ public class doorChecker : MonoBehaviour
                 doors[i].GetComponent<Animator>().SetBool("opening", false);
                 
             }
-        }   
+        }
+
+        if (doorOpen[0])
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                teleportplanes[i].SetActive(true);
+            }
+        }
+        if (doorOpen[1])
+        {
+            for (int i = 3; i < 5; i++)
+            {
+                teleportplanes[i].SetActive(true);
+            }
+        }
+        if (doorOpen[2])
+        {
+            for (int i = 5; i < 7; i++)
+            {
+                teleportplanes[i].SetActive(true);
+            }
+        }
     }
 
 }
---
+++ MED5Project/Assets/Scripts/triggerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerScript : MonoBehaviour
+{
+    public doorChecker openingDoor;
+    public int doorId;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            openingDoor.doorOpen[doorId] = true;
+        }
+    }
+}
