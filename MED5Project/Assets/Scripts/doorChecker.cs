cb39803e84704a6c37f6aa15d1ead1b2072e44c7
Dthiesen <43847647+Dthiesen@users.noreply.github.com>
2020-12-04 14:03:59 +0100

    Testing

--- MED5Project/Assets/Scripts/doorChecker.cs
+++ MED5Project/Assets/Scripts/doorChecker.cs
@@ -20,152 +20,151 @@ public class doorChecker : MonoBehaviour
 
     // Start is called before the first frame update
 
-    private void Awake()
-    {
-        testprep = GameObject.Find(testObjectName).GetComponent<twoXtwoTest>().test;
-    }
-    void Start()
-    {
-       
-        doorOpen = new bool[doors.Length];
-        teleportplanes[4].SetActive(true);
+private void Awake()
+  {
+      testprep = GameObject.Find(testObjectName).GetComponent<twoXtwoTest>().test;
+  }
+  void Start()
+  {
 
-        for (int i = 0; i < lightForObject.Length; i++)
-        {
-            lightForObject[i].SetActive(false);
-        }
+      doorOpen = new bool[doors.Length];
+      teleportplanes[4].SetActive(true);
 
-        for (int i = 0; i < audioForObject.Length; i++)
-        {
-            audioForObject[i].mute = true ;
-        }
+      for (int i = 0; i < lightForObject.Length; i++)
+      {
+          lightForObject[i].SetActive(false);
+      }
 
-        for (int i = 0; i < doors.Length; i++)
-        {
-            doors[i].GetComponent<Renderer>().materials[3].color = doorlight[1];
-            doors[i].GetComponent<Renderer>().materials[4].color = doorlight[1];
-        }
+      for (int i = 0; i < audioForObject.Length; i++)
+      {
+          audioForObject[i].mute = true ;
+      }
 
-
-
-        teleportplanes[4].SetActive(true);
-        teleportplanes[4].transform.GetChild(0).gameObject.SetActive(true);
-        teleportplanes[4].transform.GetChild(1).gameObject.SetActive(true);
-
-        //------------------for testing ---------------------------------//
-
-        if (testprep == 0)
-        {
-            for (int i = 0; i < lightForObject.Length; i++)
-            {
-                lightForObject[i].SetActive(false);
-                audioForObject[i].mute = true;
-            }
-        }
-        if (testprep == 1)
-        {
-            for (int i = 0; i < lightForObject.Length; i++)
-            {
-                audioForObject[i].mute = true;
-            }
-        }
-        if (testprep == 2)
-        {
-            for (int i = 0; i < lightForObject.Length; i++)
-            {
-                lightForObject[i].SetActive(false);
-            }
-        }
-        for (int i = 0; i < audioForObject.Length; i++)
-        {
-            audioForObject[i].mute = true;
-        }
-        //------------------for testing ---------------------------------//
-
-        lightForObject[0].SetActive(true);
-        audioForObject[0].mute = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
+      for (int i = 0; i < doors.Length; i++)
+      {
+          doors[i].GetComponent<Renderer>().materials[3].color = doorlight[1];
+          doors[i].GetComponent<Renderer>().materials[4].color = doorlight[1];
+      }
 
 
-        if (doorOpen[0])
-        {
-            teleportplanes[0].SetActive(true);
-            teleportplanes[2].SetActive(true);
 
-            //opens backLeftDoor
-            doors[0].GetComponent<Renderer>().materials[3].color = doorlight[0];
-            doors[0].GetComponent<Renderer>().materials[4].color = doorlight[0];
-            doors[0].GetComponent<Animator>().SetBool("opening", true);
+      teleportplanes[4].SetActive(true);
+      teleportplanes[4].transform.GetChild(0).gameObject.SetActive(true);
+      teleportplanes[4].transform.GetChild(1).gameObject.SetActive(true);
 
-            //opens frontRightDoor
-            doors[2].GetComponent<Renderer>().materials[3].color = doorlight[0];
-            doors[2].GetComponent<Renderer>().materials[4].color = doorlight[0];
-            doors[2].GetComponent<Animator>().SetBool("opening", true);
+      //------------------for testing ---------------------------------//
 
-            //disable sound and spotlight for laptop
-            lightForObject[0].SetActive(false);
-            interactiveObjectWithChangingMaterials[0].GetComponent<Renderer>().material = imagesForDigitalScreens[0];
+      if (testprep == 0)
+      {
+          for (int i = 0; i < lightForObject.Length; i++)
+          {
+              lightForObject[i].SetActive(false);
+              audioForObject[i].mute = true;
+          }
+      }
+      if (testprep == 1)
+      {
+          for (int i = 0; i < lightForObject.Length; i++)
+          {
+              audioForObject[i].mute = true;
+          }
+      }
+      if (testprep == 2)
+      {
+          for (int i = 0; i < lightForObject.Length; i++)
+          {
+              lightForObject[i].SetActive(false);
+          }
+      }
+      for (int i = 0; i < audioForObject.Length; i++)
+      {
+          audioForObject[i].mute = true;
+      }
+      //------------------for testing ---------------------------------//
 
-            //enable sound and light for dresser
+      lightForObject[0].SetActive(true);
+      audioForObject[0].mute = false;
+  }
 
-            lightForObject[1].SetActive(true);
+  // Update is called once per frame
+  void Update()
+  {
 
-        }
-        if (doorOpen[1])
-        {
-            teleportplanes[3].SetActive(true);
 
-            doors[1].GetComponent<Renderer>().materials[3].color = doorlight[0];
-            doors[1].GetComponent<Renderer>().materials[4].color = doorlight[0];
-            doors[1].GetComponent<Animator>().SetBool("opening", true);
+
+      if (doorOpen[0])
+      {
+          teleportplanes[0].SetActive(true);
+          teleportplanes[2].SetActive(true);
+
+          //opens backLeftDoor
+          doors[0].GetComponent<Renderer>().materials[3].color = doorlight[0];
+          doors[0].GetComponent<Renderer>().materials[4].color = doorlight[0];
+          doors[0].GetComponent<Animator>().SetBool("opening", true);
+
+          //opens frontRightDoor
+          doors[2].GetComponent<Renderer>().materials[3].color = doorlight[0];
+          doors[2].GetComponent<Renderer>().materials[4].color = doorlight[0];
+          doors[2].GetComponent<Animator>().SetBool("opening", true);
+
+          //disable sound and spotlight for laptop
+          lightForObject[0].SetActive(false);
+          interactiveObjectWithChangingMaterials[0].GetComponent<MeshRenderer>().material = imagesForDigitalScreens[0];
+
+          //enable sound and light for dresser
+
+          lightForObject[1].SetActive(true);
+
+      }
+      if (doorOpen[1])
+      {
+          teleportplanes[3].SetActive(true);
+
+          doors[1].GetComponent<Renderer>().materials[3].color = doorlight[0];
+          doors[1].GetComponent<Renderer>().materials[4].color = doorlight[0];
+          doors[1].GetComponent<Animator>().SetBool("opening", true);
 
 
             //disable sound and light for dresser
-
-            lightForObject[1].SetActive(false);
-
-
-            //enable sound and spotlight for eyescanner
-
-            lightForObject[2].SetActive(true);
-
-        }
-        if (doorOpen[2])
-        {
-
-            GameObject.Find("Spotlight FaceScanner Right").GetComponent<Light>().color = doorlight[0];
-            GameObject.Find("Spotlight FaceScanner Left").GetComponent<Light>().color = doorlight[0];
+            lightForObject[1].GetComponent<Animator>().SetBool("triggered",true);
 
 
-            //disable sound and spotlight for eyescanner
+          //enable sound and spotlight for eyescanner
 
-            lightForObject[2].SetActive(false);
+          lightForObject[2].SetActive(true);
 
-            //enable sound and light for hand scanner
+      }
+      if (doorOpen[2])
+      {
 
-            lightForObject[3].SetActive(true);
-
-            teleportplanes[1].SetActive(true);
-            doors[3].GetComponent<Renderer>().materials[3].color = doorlight[0];
-            doors[3].GetComponent<Renderer>().materials[4].color = doorlight[0];
-            doors[3].GetComponent<Animator>().SetBool("opening", true);
-        }
-
-        if (doorOpen[3])
-        {
-            //disable sound and spotlight for handscanner
-
-            lightForObject[3].SetActive(false);
-            interactiveObjectWithChangingMaterials[1].GetComponent<Renderer>().material = imagesForDigitalScreens[1];
-        }
-       
+          GameObject.Find("Spotlight FaceScanner Right").GetComponent<Light>().color = doorlight[0];
+          GameObject.Find("Spotlight FaceScanner Left").GetComponent<Light>().color = doorlight[0];
 
 
-    }
+          //disable sound and spotlight for eyescanner
+
+          lightForObject[2].SetActive(false);
+
+          //enable sound and light for hand scanner
+
+          lightForObject[3].SetActive(true);
+
+          teleportplanes[1].SetActive(true);
+          doors[3].GetComponent<Renderer>().materials[3].color = doorlight[0];
+          doors[3].GetComponent<Renderer>().materials[4].color = doorlight[0];
+          doors[3].GetComponent<Animator>().SetBool("opening", true);
+      }
+
+      if (doorOpen[3])
+      {
+          //disable sound and spotlight for handscanner
+
+          lightForObject[3].SetActive(false);
+          interactiveObjectWithChangingMaterials[1].GetComponent<MeshRenderer>().material = imagesForDigitalScreens[1];
+      }
+
+
+
+  }
 
 }
