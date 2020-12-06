0983b4edd75bba794f80e7898804b7e3444c4176
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2020-12-07 00:07:00 +0100

    updated the timer script
    can now give the interval time and print them

--- MED5Project/Assets/Scripts/TrackingFromHeadset.cs
+++ MED5Project/Assets/Scripts/TrackingFromHeadset.cs
@@ -1,31 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class TrackingFromHeadset : MonoBehaviour
 {
     public Camera main;
     public string tagLookingFor;
     public float totalTimeSpent;
+    public float startTime;
     public float[] lookTimeForObject;
+    public float[] intervalTimer;
+    public bool[] triggersForIntervalTime;
     public GameObject[] interactiveObjects;
+    int textCounter;
 
     public Transform _selection;
-    
+
 
 
     private void Awake()
     {
         lookTimeForObject = new float[interactiveObjects.Length];
+        intervalTimer = new float[interactiveObjects.Length];
+        triggersForIntervalTime = new bool[interactiveObjects.Length];
         main = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
-
-
+    private void Start()
+    {
+        startTime = Time.time;
+    }
     // Update is called once per frame
     void Update()
     {
         totalTimeSpent += Time.deltaTime;
+
+       for (int i = 0; i < triggersForIntervalTime.Length; i++)
+        {
+            if (!triggersForIntervalTime[i])
+            {
+                intervalTimer[i] = totalTimeSpent;
+            }            
+        }
+
+        if (triggersForIntervalTime[7])
+        {
+           writeFile();
+        }
+
+
         Ray ray =  new Ray(main.transform.position,main.transform.forward);
         RaycastHit hit;
 
@@ -43,6 +68,42 @@ public class TrackingFromHeadset : MonoBehaviour
                 }
             }
         }
-        Debug.DrawRay(main.transform.position,main.transform.forward,Color.black,1);
+       
+    }
+
+    void writeFile()
+    {
+        string path = Application.dataPath + "/TestData.txt";
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path,"DataSet from Test \n\n");
+        }
+        File.AppendAllText(path, "------------------------------------------------\n");
+        File.AppendAllText(path, "Total time spent: " + totalTimeSpent.ToString()+ "\n\n");
+
+        for (int i = 0; i < interactiveObjects.Length; i++)
+        {
+            File.AppendAllText(path, "Time looked at "+interactiveObjects[i].name +": " + lookTimeForObject[i].ToString() + "\n");
+        }
+
+        File.AppendAllText(path, "\n");
+        for (int i = 0; i < interactiveObjects.Length; i++)
+        {
+            if (i == 0)
+            {
+                File.AppendAllText(path, "interval time " + i + ": " + intervalTimer[i].ToString() + "\n");
+            }
+            else
+            {
+                float intervalTime = intervalTimer[i] - intervalTimer[i - 1];
+                File.AppendAllText(path, "interval time " + i + ": " + intervalTime.ToString() + "\n");
+            }
+            
+        }
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+        
+        
     }
 }
